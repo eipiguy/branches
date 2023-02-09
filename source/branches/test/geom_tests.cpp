adefbaf37ddbf6d2305c6e9e2a11618a33eddecd
Neal Nesbitt <ndnesbitt@dnesbitt.com>
2023-02-09 06:27:34 -0800

    added google test and basic hello world test

---
+++ source/branches/test/geom_tests.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+
+TEST(geom_tests, point_test) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
